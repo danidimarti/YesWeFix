af70cf18f3f290d957a5bcb421233f8167cfeb07
Dani <dani.dimarti@gmail.com>
2019-06-01 13:35:50 +0200

    shopmodel & shop form

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 //import MapRender from './components/MapRender';
 import SearchBar from './components/SearchBar'
+import Shop from '../src/components/Shop/Shop'
 
 function App() {
   return (
     <div className="App">
       <SearchBar />
+      <Shop />
     </div>
   );
 }
