60de85689841d1523b312c3023c97646732b6ce6
Dani <dani.dimarti@gmail.com>
2019-06-13 20:32:18 +0200

    map render done and search done

--- frontend-react/src/components/Map/MapRender.js
+++ frontend-react/src/components/Map/MapRender.js
@@ -8,66 +8,127 @@ import {
   InfoWindow
 } from "react-google-maps";
 //we need to import the data model form repair shops
-import * as shopData from "../Shop/shop-data/shoplist-data.json";
+import shopData from "../Shop/shop-data/shoplist-data.js";
 import MapStyle from "./MapStyle.js";
+import pin from "./pin-sm.png";
 
 class MapRender extends Component {
-  state = {
-    location: this.props.location
-  };
-  render() {    
+  constructor(props){
+    super(props);
+    this.state = {
+      location: this.props.location,
+      selectedShop: null,
+      shopData: shopData,
+      bounds: null,
+      loading: true
+      // map: React.createRef()
     
+    };
+    this.map = React.createRef();
+  }
+  
+  
+  componentDidMount(){
+    this.setState({
+      loading: false
+    })
+  }
+  // selectedShop = shop => {
+  //   this.setState({
+  //     selectedShop: shop
+  //   });
+
+  // };
+
+  setSelectedShop = shop => {
+    console.log(shop);
+    //const { value, name } = e.target;
+    this.setState({
+      selectedShop: shop
+    });
+  };
+
+  setBounds = () => {
+    this.setState({
+      bounds: true
+    });
+  };
+
+  render() { 
+    //const bounds = new window.google.maps.LatLngBounds()
     const WrappedMap = withScriptjs(
       withGoogleMap(props => (
-        <GoogleMap defaultZoom={15} defaultCenter={this.state.location} />
+        <GoogleMap 
+        ref={map => {
+          this.map = map;
+        }}
+        onIdle={props.onMapIdle}
+        defaultZoom={15} 
+        defaultCenter={this.state.location} >
+          {this.state.shopData.map((shop, index) => (
+            <Marker
+              key={shop.id}
+              position={{
+                lat: parseFloat(shop.lat),
+                lng: parseFloat(shop.lng)
+              }}
+              onClick={() => {
+                this.setSelectedShop(shop);
+              }}
+              icon={pin}
+              animation={window.google.maps.Animation.DROP}
+            />
+          ))}
+          {this.state.selectedShop && (
+            <InfoWindow
+              position={{
+                lat: parseFloat(this.state.selectedShop.lat),
+                lng: parseFloat(this.state.selectedShop.lng)
+              }}
+              onCloseClick={() => {
+                this.setSelectedShop(null);
+              }}
+            >
+              <div>
+                <h2 style={{ fontSize: 18 }}>
+                  {this.state.selectedShop.businessname}
+                </h2>
+                <p
+                  style={{ fontStyle: "italic", fontWeight: 400, fontSize: 12 }}
+                >
+                  {this.state.selectedShop.repairtype}
+                </p>
+                <img
+                  src={this.state.selectedShop.imageUrl}
+                  alt="Selected Shop"
+                  style={{ width: 200 }}
+                />
+              </div>
+            </InfoWindow>
+          )}
+        </GoogleMap>
       ))
     );
-    console.log(this.props.location)
+
     return (
       <div>
-        <WrappedMap
+       {!this.state.loading ?  <WrappedMap
+         onMapIdle={() => {
+          let ne = this.map.getBounds().getNorthEast();
+          let sw = this.map.getBounds().getSouthWest();
+          console.log("Northeast bound is:", ne.lat() + ";" + ne.lng());
+          console.log("Southwest bound is:", sw.lat() + ";" + sw.lng());
+        }}
           googleMapURL={`${process.env.REACT_APP_GG_URL}`}
           //which elements (divs) is it going to place the map inside
-          loadingElement={<div style={{  height: `100%` }} />}
+          loadingElement={<div style={{ height: `100%` }} />}
           containerElement={<div style={{ width: "350px", height: `100%` }} />}
           mapElement={<div style={{ height: `500px` }} />}
           wrappedMap={WrappedMap}
-        />
+        /> : ""}
       </div>
     );
   }
 }
 
 export default MapRender;
-
-/* {shopData.features.map(park => (
-              <Marker
-                key={park.properties.PARK_ID}
-                position={{
-                  lat: park.geometry.coordinates[1],
-                  lng: park.geometry.coordinates[0]
-                }}
-                onClick={() => {
-                  setSelectedShop(park);
-                }}
-                icon={{url:"../images/tool.svg", 
-                scaledSize: new window.google.maps.Size(25, 25)
-              }} */
-/* />
-            ))} */
-/* {selectedShop && (
-              <InfoWindow
-                position={{
-                  lat: selectedShop.geometry.coordinates[1],
-                  lng: selectedShop.geometry.coordinates[0]
-                }}
-                onCloseClick={() => {
-                  setSelectedShop(null);
-                }}
-              >
-                <div>
-                    <h2>{selectedShop.properties.NAME}</h2>
-                    <p>{selectedShop.properties.DESCRIPTIO}</p>
-                </div>
-              </InfoWindow>
-            )} */
--- frontend-react/src/components/Results/Results.js
+++ frontend-react/src/components/Results/Results.js
@@ -1,20 +1,15 @@
-import React, { Component } from 'react'
-import {Route, Switch, Link} from 'react-router-dom'
-import MapRender from '../../components/Map/MapRender'
+import React, { Component } from "react";
+import { Route, Switch, Link } from "react-router-dom";
+import MapRender from "../../components/Map/MapRender";
 
 export class Results extends Component {
-    render() {
-        return (
-            <div>
-              <div
-              
-              > It's Aliveeeeeee!</div> 
-              <MapRender 
-            location={this.props.location}
-              />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <MapRender location={this.props.location} />
+      </div>
+    );
+  }
 }
 
-export default Results
+export default Results;
